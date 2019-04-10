fdb217f89ea1b8061e0624528ad8059d57ef7c11
David <djtover94@gmail.com>
2019-04-10 22:36:37 +0300

    added some more tests

--- PhysicalNumberTest.cpp
+++ PhysicalNumberTest.cpp
@@ -24,6 +24,18 @@ int main() {
     PhysicalNumber c(2, Unit::HOUR);
     PhysicalNumber d(30, Unit::MIN);
 
+    // My values
+    PhysicalNumber x1(4, Unit::CM);
+    PhysicalNumber x2(4, Unit::M);
+    PhysicalNumber x3(4, Unit::KM);
+    PhysicalNumber x4(4, Unit::SEC);
+    PhysicalNumber x5(4, Unit::MIN);
+    PhysicalNumber x6(4, Unit::HOUR);
+    PhysicalNumber x7(4, Unit::G);
+    PhysicalNumber x8(4, Unit::KG);
+    PhysicalNumber x9(4, Unit::TON);
+
+
     testcase
     .setname("Basic output")
     .CHECK_OUTPUT(a, "2[km]")
@@ -51,7 +63,182 @@ int main() {
 
     // YOUR TESTS - INSERT AS MANY AS YOU WANT
 
-      .setname("...")
+      .setname("Compatible Dimensions on meters")
+      .CHECK_OUTPUT(x1,"4[cm]")
+      .CHECK_OUTPUT(x2,"4[m]")
+      .CHECK_OUTPUT(x3,"4[km]")
+      .CHECK_OUTPUT(x1+x2,"404[cm]")
+      .CHECK_OUTPUT(x1+x3,"400004[cm]")
+      .CHECK_OUTPUT(x2+x1,"4.04[m]")
+      .CHECK_OUTPUT(x2+x3,"4004[m]")
+      .CHECK_OUTPUT(x3+x1,"4.00004[km]")
+      .CHECK_OUTPUT(x3+x2,"4.004[km]")
+
+      .CHECK_OUTPUT(x1-x2,"-396[cm]")
+      .CHECK_OUTPUT(x1-x3,"-399996[cm]")
+      .CHECK_OUTPUT(x2-x1,"3.96[m]")
+      .CHECK_OUTPUT(x2-x3,"-3996[m]")
+      .CHECK_OUTPUT(x3-x1,"3.99996[km]")
+      .CHECK_OUTPUT(x3-x2,"3.996[km]")
+      
+    .setname("Incompatible dimensions from meters")
+    .CHECK_THROWS(x1+x4)
+    .CHECK_THROWS(x1+x5)
+    .CHECK_THROWS(x1+x6)
+    .CHECK_THROWS(x1+x7)
+    .CHECK_THROWS(x1+x8)
+    .CHECK_THROWS(x1+x9)
+    .CHECK_THROWS(x2+x4)
+    .CHECK_THROWS(x2+x5)
+    .CHECK_THROWS(x2+x6)
+    .CHECK_THROWS(x2+x7)
+    .CHECK_THROWS(x2+x8)
+    .CHECK_THROWS(x2+x9)
+    .CHECK_THROWS(x3+x4)
+    .CHECK_THROWS(x3+x5)
+    .CHECK_THROWS(x3+x6)
+    .CHECK_THROWS(x3+x7)
+    .CHECK_THROWS(x3+x8)
+    .CHECK_THROWS(x3+x9)
+
+
+    .CHECK_THROWS(x1-x4)
+    .CHECK_THROWS(x1-x5)
+    .CHECK_THROWS(x1-x6)
+    .CHECK_THROWS(x1-x7)
+    .CHECK_THROWS(x1-x8)
+    .CHECK_THROWS(x1-x9)
+    .CHECK_THROWS(x2-x4)
+    .CHECK_THROWS(x2-x5)
+    .CHECK_THROWS(x2-x6)
+    .CHECK_THROWS(x2-x7)
+    .CHECK_THROWS(x2-x8)
+    .CHECK_THROWS(x2-x9)
+    .CHECK_THROWS(x3-x4)
+    .CHECK_THROWS(x3-x5)
+    .CHECK_THROWS(x3-x6)
+    .CHECK_THROWS(x3-x7)
+    .CHECK_THROWS(x3-x8)
+    .CHECK_THROWS(x3-x9)
+
+       .setname("Compatible Dimensions on time")
+      .CHECK_OUTPUT(x4,"4[sec]")
+      .CHECK_OUTPUT(x5,"4[min]")
+      .CHECK_OUTPUT(x6,"4[hour]")
+      .CHECK_OUTPUT(x4+x5,"244[sec]")
+      .CHECK_OUTPUT(x4+x6,"14404[sec]")
+      .CHECK_OUTPUT(x5+x4,"4.066666[min]")
+      .CHECK_OUTPUT(x5+x6,"244[min]")
+      .CHECK_OUTPUT(x6+x4,"4.001111[hour]")
+      .CHECK_OUTPUT(x6+x5,"4.066666[hour]")
+
+      .CHECK_OUTPUT(x4-x5,"-236[sec]")
+      .CHECK_OUTPUT(x4-x6,"-14396[sec]")
+      .CHECK_OUTPUT(x5-x4,"3.933334[min]")
+      .CHECK_OUTPUT(x5-x6,"-14396[min]")
+      .CHECK_OUTPUT(x6-x4,"3.998889[hour]")
+      .CHECK_OUTPUT(x6-x5,"3.933334[hour]")
+
+    .setname("Incompatible dimensions from time")
+    .CHECK_THROWS(x4+x1)
+    .CHECK_THROWS(x4+x2)
+    .CHECK_THROWS(x4+x3)
+    .CHECK_THROWS(x4+x7)
+    .CHECK_THROWS(x4+x8)
+    .CHECK_THROWS(x4+x9)
+    .CHECK_THROWS(x5+x1)
+    .CHECK_THROWS(x5+x2)
+    .CHECK_THROWS(x5+x3)
+    .CHECK_THROWS(x5+x7)
+    .CHECK_THROWS(x5+x8)
+    .CHECK_THROWS(x5+x9)
+    .CHECK_THROWS(x6+x1)
+    .CHECK_THROWS(x6+x2)
+    .CHECK_THROWS(x6+x3)
+    .CHECK_THROWS(x6+x7)
+    .CHECK_THROWS(x6+x8)
+    .CHECK_THROWS(x6+x9)
+
+
+    .CHECK_THROWS(x4-x1)
+    .CHECK_THROWS(x4-x2)
+    .CHECK_THROWS(x4-x3)
+    .CHECK_THROWS(x4-x7)
+    .CHECK_THROWS(x4-x8)
+    .CHECK_THROWS(x4-x9)
+    .CHECK_THROWS(x5-x1)
+    .CHECK_THROWS(x5-x2)
+    .CHECK_THROWS(x5-x3)
+    .CHECK_THROWS(x5-x7)
+    .CHECK_THROWS(x5-x8)
+    .CHECK_THROWS(x5-x9)
+    .CHECK_THROWS(x6-x1)
+    .CHECK_THROWS(x6-x2)
+    .CHECK_THROWS(x6-x3)
+    .CHECK_THROWS(x6-x7)
+    .CHECK_THROWS(x6-x8)
+    .CHECK_THROWS(x6-x9) 
+
+           .setname("Compatible Dimensions on time")
+      .CHECK_OUTPUT(x7,"4[g]")
+      .CHECK_OUTPUT(x8,"4[kg]")
+      .CHECK_OUTPUT(x9,"4[ton]")
+      .CHECK_OUTPUT(x7+x8,"4004[g]")
+      .CHECK_OUTPUT(x7+x9,"4000004[g]")
+      .CHECK_OUTPUT(x8+x7,"4.004[kg]")
+      .CHECK_OUTPUT(x8+x9,"4004[kg]")
+      .CHECK_OUTPUT(x9+x7,"4.000004[ton]")
+      .CHECK_OUTPUT(x9+x8,"4.004[ton]")
+
+      .CHECK_OUTPUT(x7-x8,"-3996[g]")
+      .CHECK_OUTPUT(x7-x9,"-3999996[g]")
+      .CHECK_OUTPUT(x8-x7,"3.996[kg]")
+      .CHECK_OUTPUT(x8-x9,"-3996[kg]")
+      .CHECK_OUTPUT(x9-x7,"3.999996[ton]")
+      .CHECK_OUTPUT(x9-x8,"3.996[ton]")
+
+    .setname("Incompatible dimensions from grams")
+    .CHECK_THROWS(x7+x4)
+    .CHECK_THROWS(x7+x5)
+    .CHECK_THROWS(x7+x6)
+    .CHECK_THROWS(x7+x1)
+    .CHECK_THROWS(x7+x2)
+    .CHECK_THROWS(x7+x3)
+    .CHECK_THROWS(x8+x4)
+    .CHECK_THROWS(x8+x5)
+    .CHECK_THROWS(x8+x6)
+    .CHECK_THROWS(x8+x1)
+    .CHECK_THROWS(x8+x2)
+    .CHECK_THROWS(x8+x3)
+    .CHECK_THROWS(x9+x4)
+    .CHECK_THROWS(x9+x5)
+    .CHECK_THROWS(x9+x6)
+    .CHECK_THROWS(x9+x1)
+    .CHECK_THROWS(x9+x2)
+    .CHECK_THROWS(x9+x3)
+
+
+    .CHECK_THROWS(x1-x4)
+    .CHECK_THROWS(x1-x5)
+    .CHECK_THROWS(x1-x6)
+    .CHECK_THROWS(x1-x7)
+    .CHECK_THROWS(x1-x8)
+    .CHECK_THROWS(x1-x9)
+    .CHECK_THROWS(x2-x4)
+    .CHECK_THROWS(x2-x5)
+    .CHECK_THROWS(x2-x6)
+    .CHECK_THROWS(x2-x7)
+    .CHECK_THROWS(x2-x8)
+    .CHECK_THROWS(x2-x9)
+    .CHECK_THROWS(x3-x4)
+    .CHECK_THROWS(x3-x5)
+    .CHECK_THROWS(x3-x6)
+    .CHECK_THROWS(x3-x7)
+    .CHECK_THROWS(x3-x8)
+    .CHECK_THROWS(x3-x9)     
+      
+
+     
 
       .print(cout, /*show_grade=*/false);
       grade = testcase.grade();
