1dd577c46d244b828e8d89d02b92b473e4faf721
David <djtover94@gmail.com>
2019-04-12 16:21:31 +0300

    Added to set type

--- PhysicalNumber.h
+++ PhysicalNumber.h
@@ -68,7 +68,6 @@ public:
     else if (s == "kg]")
     {
       type = Unit::KG;
-      // cout << (int)this->getType() << " is type num"<<endl;
     }
     else if (s == "ton]")
     {
@@ -81,126 +80,126 @@ public:
   }
   void setType(Unit u)
   {
-    type = u;
-  }
-
-  PhysicalNumber operator+(PhysicalNumber other);
-
-  PhysicalNumber operator+();
-
-  PhysicalNumber operator+=(PhysicalNumber other);
-
-  PhysicalNumber operator++();
-
-  PhysicalNumber operator++(int other);
-
-  PhysicalNumber operator-(PhysicalNumber other);
-
-  PhysicalNumber operator-();
-
-  PhysicalNumber operator-=(PhysicalNumber other);
-
-  PhysicalNumber operator--();
-
-  PhysicalNumber operator--(int other);
-
-  bool operator<(PhysicalNumber other);
-
-  bool operator<=(PhysicalNumber other);
-
-  bool operator>(PhysicalNumber other);
-
-  bool operator>=(PhysicalNumber other);
-
-  bool operator==(PhysicalNumber other);
-
-  bool operator!=(PhysicalNumber other);
-
-  friend ostream &operator<<(ostream &os, PhysicalNumber pn)
-  {
-    os.precision(6);
-    double v = pn.getValue();
-    int s = (int)pn.getType();
-    if (s == 0)
+    if (u == Unit::CM || u == Unit::M ||
+        u == Unit::KM || u == Unit::SEC || u == Unit::MIN ||
+        u == Unit::HOUR || u == Unit::G || u == Unit::KG || u == Unit::TON)
     {
-      return (os << v << "[cm]");
-    }
-    else if (s == 1)
-    {
-      return (os << v << "[m]");
-    }
-    else if (s == 2)
-    {
-      return (os << v << "[km]");
-    }
-    else if (s == 3)
-    {
-      return (os << v << "[sec]");
-    }
-    else if (s == 4)
-    {
-      return (os << v << "[min]");
-    }
-    else if (s == 5)
-    {
-      return (os << v << "[hour]");
-    }
-    else if (s == 6)
-    {
-      return (os << v << "[g]");
-    }
-    else if (s == 7)
-    {
-      return (os << v << "[kg]");
-    }
-    else if (s == 8)
-    {
-      return (os << v << "[ton]");
-    }
+    
+      type = u;
+     }
     else
-    {
-      throw std::out_of_range("Thats not a type");
+     {
+    throw std::out_of_range("Thats not a type");
     }
-  }
-  friend istream &operator>>(istream &input, PhysicalNumber &pn)
+}
+
+PhysicalNumber
+operator+(PhysicalNumber other);
+
+PhysicalNumber operator+();
+
+PhysicalNumber operator+=(PhysicalNumber other);
+
+PhysicalNumber operator++();
+
+PhysicalNumber operator++(int other);
+
+PhysicalNumber operator-(PhysicalNumber other);
+
+PhysicalNumber operator-();
+
+PhysicalNumber operator-=(PhysicalNumber other);
+
+PhysicalNumber operator--();
+
+PhysicalNumber operator--(int other);
+
+bool operator<(PhysicalNumber other);
+
+bool operator<=(PhysicalNumber other);
+
+bool operator>(PhysicalNumber other);
+
+bool operator>=(PhysicalNumber other);
+
+bool operator==(PhysicalNumber other);
+
+bool operator!=(PhysicalNumber other);
+
+friend ostream &operator<<(ostream &os, PhysicalNumber pn)
+{
+  os.precision(6);
+  double v = pn.getValue();
+  int s = (int)pn.getType();
+  if (s == 0)
   {
-    double newValue;
-    string newType;
+    return (os << v << "[cm]");
+  }
+  else if (s == 1)
+  {
+    return (os << v << "[m]");
+  }
+  else if (s == 2)
+  {
+    return (os << v << "[km]");
+  }
+  else if (s == 3)
+  {
+    return (os << v << "[sec]");
+  }
+  else if (s == 4)
+  {
+    return (os << v << "[min]");
+  }
+  else if (s == 5)
+  {
+    return (os << v << "[hour]");
+  }
+  else if (s == 6)
+  {
+    return (os << v << "[g]");
+  }
+  else if (s == 7)
+  {
+    return (os << v << "[kg]");
+  }
+  else if (s == 8)
+  {
+    return (os << v << "[ton]");
+  }
+  else
+  {
+    throw std::out_of_range("Thats not a type");
+  }
+}
+friend istream &operator>>(istream &input, PhysicalNumber &pn)
+{
+  double newValue;
+  string newType;
 
-    // remember place for rewinding
-    ios::pos_type startPosition = input.tellg();
+  // remember place for rewinding
+  ios::pos_type startPosition = input.tellg();
 
-    if ((!(input >> newValue)) ||
-        (!getAndCheckNextCharIs(input, '[')) ||
-        (!(input >> newType))) //||
-                               //(!getAndCheckNextCharIs(input, ']')))
-    {
-
-      // cout<< newValue<< "is new value"<< endl;
-      // cout<< newType<< "is new value"<< endl;
-      // rewind on error
-      auto errorState = input.rdstate(); // remember error state
-      input.clear();                     // clear error so seekg will work
-      input.seekg(startPosition);        // rewind
-      input.clear(errorState);           // set back the error flag
-    }
-    else
-    {
-      pn.setType(newType);
-      pn.setValue(newValue);
-      
-      // cout<< pn<< "is new value"<< endl;
-      // cout<< pn.getType()<< "is new value"<< endl;
-    }
-
-    return input;
+  if ((!(input >> newValue)) ||
+      (!getAndCheckNextCharIs(input, '[')) ||
+      (!(input >> newType))) //||
+                             //(!getAndCheckNextCharIs(input, ']')))
+  {
+    // rewind on error
+    auto errorState = input.rdstate(); // remember error state
+    input.clear();                     // clear error so seekg will work
+    input.seekg(startPosition);        // rewind
+    input.clear(errorState);           // set back the error flag
+  }
+  else
+  {
+    pn.setType(newType);
+    pn.setValue(newValue);
   }
 
-  // PhysicalNumber(double x, ariel::Unit u)
-  // {
-  //   value = x;
-  //   type = u;
-  // }
-  // ~PhysicalNumber();
-};
+  return input;
+}
+// ~PhysicalNumber();
 }; // namespace ariel
+}
+; // namespace ariel
